dfea633eac273d4dbf6a424364e0740659af9601
Siddhartha (2049848) <np03A190145@heraldcollege.edu.np>
2021-11-23 10:13:30 +0545

    cinfiguration and setup for postgresql database

--- libymaxapp/views.py
+++ libymaxapp/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, auth
+
 
 def register(request):
     return render(request, "login.html")
+    # if request.method == "POST":
+    #     Fullname=request.POST['name'],
+    #     email=request.POST['email'],        
+    #     password=request.POST['pass'],                
+
+    #     user= User.objects.create_user()
+    #     else:
+    #         return render(request, "login.html")    
