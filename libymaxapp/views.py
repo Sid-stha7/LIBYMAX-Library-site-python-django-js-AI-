f999b2cced14b11c03b59f6fe4f7ad3977d5f2e4
Siddhartha (2049848) <np03A190145@heraldcollege.edu.np>
2021-11-23 11:20:15 +0545

    login info is validated into database

--- libymaxapp/views.py
+++ libymaxapp/views.py
@@ -1,14 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.models import User, auth
 
 
-def register(request):
-    return render(request, "login.html")
-    # if request.method == "POST":
-    #     Fullname=request.POST['name'],
-    #     email=request.POST['email'],        
-    #     password=request.POST['pass'],                
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
-    #     user= User.objects.create_user()
-    #     else:
-    #         return render(request, "login.html")    
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("index")
+        else:
+            messages.info(request, 'invalid credentials')
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html')
+
+
+def register(request):
+
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(
+                    username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                print('user created')
+                return redirect('login')
+
+        else:
+            messages.info(request, 'password not matching..')
+            return redirect('register')
+        return redirect('/')
+
+    else:
+        return render(request, 'register.html')
+
