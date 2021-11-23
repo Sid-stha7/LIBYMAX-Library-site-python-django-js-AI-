43618eadb93e15029bdacb826ea43ff102050a24
Siddhartha (2049848) <np03A190145@heraldcollege.edu.np>
2021-11-23 19:58:10 +0545

    repeated username is warning fix

--- libymaxapp/views.py
+++ libymaxapp/views.py
@@ -1,22 +1,45 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User, auth
-
+from django.contrib import messages
 
 
 def register(request):
     if request.method == 'POST':
-        username = request.POST['name']   
+        
+        username = request.POST['username']   
         password = request.POST['password']
         email = request.POST['email']
-        
-        user = User.objects.create_user(username=username, password=password, email=email)
-        user.save()
-        print('user created')
+
        
+        if User.objects.filter(username=username).exists():
+            print("Username taken")
+            return render(request, 'login.html')   
+        else:
+         user = User.objects.create_user(username=username, password=password, email=email)
+         user.save()
+         print('user created')
+         return render(request, 'login.html')
     
     else:
+        print("retry again")
         return render(request, 'login.html')
     
         
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("register")
+        else:
+            messages.info(request, 'invalid credentials')
+            return redirect('register')
+
+    else:
+        return render(request, 'login.html')
 
     
