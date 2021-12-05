e69d31c19c0cee954246f290b7c61aba59fc5633
Siddhartha (2049848) <np03A190145@heraldcollege.edu.np>
2021-12-05 13:55:29 +0545

    pushed in branch

--- libymaxapp/views.py
+++ libymaxapp/views.py
@@ -3,43 +3,59 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
 
-def register(request):
-    if request.method == 'POST':
-        
-        username = request.POST['username']   
-        password = request.POST['password']
-        email = request.POST['email']
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
 
-       
-        if User.objects.filter(username=username).exists():
-            print("Username taken")
-            return render(request, 'login.html')   
-        else:
-         user = User.objects.create_user(username=username, password=password, email=email)
-         user.save()
-         print('user created')
-         return render(request, 'login.html')
-    
-    else:
-        print("retry again")
-        return render(request, 'login.html')
-    
-        
+# Create your views here.
+
+
 def login(request):
-    if request.method == 'POST':
-        email = request.POST['email']
+    if request.method== 'POST':
+        username = request.POST['username']
         password = request.POST['password']
 
-        user = auth.authenticate(email=email, password=password)
+        user = auth.authenticate(username=username,password=password)
 
         if user is not None:
             auth.login(request, user)
             return redirect("register")
         else:
-            messages.info(request, 'invalid credentials')
-            return redirect('register')
+            messages.info(request,'invalid credentials')
+            return redirect('login')
 
     else:
-        return render(request, 'login.html')
+        return render(request,'login.html')    
 
-    
+def register(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'Username Taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'Email Taken')
+                return redirect('register')
+            else:   
+                user = User.objects.create_user(username=username, password=password1, email=email)
+                user.save()
+                print('user created')
+                return redirect('login')
+
+        else:
+            messages.info(request,'password not matching..')    
+            return redirect('register')
+        
+        
+    else:
+        return render(request,'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('register') 
